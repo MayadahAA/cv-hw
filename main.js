4c546609bee981d82d83efd8713b7bc2f11606bc
Mayadah <97852885+MayadahAA@users.noreply.github.com>
2023-08-21 06:48:39 +0300

    commit

---
+++ main.js
@@ -0,0 +1,174 @@
+const react1 = document.getElementById("root");
+
+let element = (
+  <>
+    <h1 className="name">Mayadah Alarifi</h1>
+    <p className="subtitle">Frontend Developer</p>
+
+    <ul className="contact">
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 512 512"
+        >
+          <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+        </svg>
+        alarifimayadah@gmail.com |
+      </li>
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+        >
+          <path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm90.7 96.7c9.7-2.6 19.9 2.3 23.7 11.6l20 48c3.4 8.2 1 17.6-5.8 23.2L168 231.7c16.6 35.2 45.1 63.7 80.3 80.3l20.2-24.7c5.6-6.8 15-9.2 23.2-5.8l48 20c9.3 3.9 14.2 14 11.6 23.7l-12 44C336.9 378 329 384 320 384C196.3 384 96 283.7 96 160c0-9 6-16.9 14.7-19.3l44-12z" />
+        </svg>
+        0580113659 |
+      </li>
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+        >
+          <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+        </svg>
+        @Mayadah-Alarifi |
+      </li>
+      <li>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+        >
+          <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zM277.3 415.7c-8.4 1.5-11.5-3.7-11.5-8 0-5.4.2-33 .2-55.3 0-15.6-5.2-25.5-11.3-30.7 37-4.1 76-9.2 76-73.1 0-18.2-6.5-27.3-17.1-39 1.7-4.3 7.4-22-1.7-45-13.9-4.3-45.7 17.9-45.7 17.9-13.2-3.7-27.5-5.6-41.6-5.6-14.1 0-28.4 1.9-41.6 5.6 0 0-31.8-22.2-45.7-17.9-9.1 22.9-3.5 40.6-1.7 45-10.6 11.7-15.6 20.8-15.6 39 0 63.6 37.3 69 74.3 73.1-4.8 4.3-9.1 11.7-10.6 22.3-9.5 4.3-33.8 11.7-48.3-13.9-9.1-15.8-25.5-17.1-25.5-17.1-16.2-.2-1.1 10.2-1.1 10.2 10.8 5 18.4 24.2 18.4 24.2 9.7 29.7 56.1 19.7 56.1 19.7 0 13.9.2 36.5.2 40.6 0 4.3-3 9.5-11.5 8-66-22.1-112.2-84.9-112.2-158.3 0-91.8 70.2-161.5 162-161.5S388 165.6 388 257.4c.1 73.4-44.7 136.3-110.7 158.3zm-98.1-61.1c-1.9.4-3.7-.4-3.9-1.7-.2-1.5 1.1-2.8 3-3.2 1.9-.2 3.7.6 3.9 1.9.3 1.3-1 2.6-3 3zm-9.5-.9c0 1.3-1.5 2.4-3.5 2.4-2.2.2-3.7-.9-3.7-2.4 0-1.3 1.5-2.4 3.5-2.4 1.9-.2 3.7.9 3.7 2.4zm-13.7-1.1c-.4 1.3-2.4 1.9-4.1 1.3-1.9-.4-3.2-1.9-2.8-3.2.4-1.3 2.4-1.9 4.1-1.5 2 .6 3.3 2.1 2.8 3.4zm-12.3-5.4c-.9 1.1-2.8.9-4.3-.6-1.5-1.3-1.9-3.2-.9-4.1.9-1.1 2.8-.9 4.3.6 1.3 1.3 1.8 3.3.9 4.1zm-9.1-9.1c-.9.6-2.6 0-3.7-1.5s-1.1-3.2 0-3.9c1.1-.9 2.8-.2 3.7 1.3 1.1 1.5 1.1 3.3 0 4.1zm-6.5-9.7c-.9.9-2.4.4-3.5-.6-1.1-1.3-1.3-2.8-.4-3.5.9-.9 2.4-.4 3.5.6 1.1 1.3 1.3 2.8.4 3.5zm-6.7-7.4c-.4.9-1.7 1.1-2.8.4-1.3-.6-1.9-1.7-1.5-2.6.4-.6 1.5-.9 2.8-.4 1.3.7 1.9 1.8 1.5 2.6z" />
+        </svg>
+        @MayadahAA
+      </li>
+    </ul>
+
+    <p className="summ">
+      A Graduate of computer science with experience working across the frontend
+      development. Looking for a role where I can grow and learn from
+      experienced team members while drawing on project.
+    </p>
+    <div className="continer">
+      <div className="left-column">
+        <div className="skills">
+          <h2>Skills</h2>
+          <hr></hr>
+
+          <h4>Soft Skills</h4>
+          <ul>
+            <li>Team collaboration</li>
+            <li>Problem solving</li>
+            <li>Self-learning.</li>
+            <li>Analytics skills</li>
+          </ul>
+          <h4>Tech Skills</h4>
+          <ul>
+            <li>HTML & CSS and JavaScript</li>
+            <li>UX & UI, Web Design.</li>
+            <li>Website optimization</li>
+            <li>SEO proficiency</li>
+          </ul>
+        </div>
+        <h2>Languages</h2>
+        <hr></hr>
+
+        <ul>
+          <li>Arabic</li>
+          <li>English</li>
+        </ul>
+
+        <div className="projects">
+          <h2>Projects</h2>
+          <hr></hr>
+
+          <ul>
+            <li>Weather forecast site using JavaScript.</li>
+            <li>SIHI clone app design using Figma.</li>
+            <li>Tech events UX research and designed it using Figma.</li>
+          </ul>
+        </div>
+        <div className="cert">
+          <h2>Certification</h2>
+          <hr></hr>
+
+          <ul>
+            <li>
+              Data-Driven Decision making. <b>Sep – 2022</b>{" "}
+            </li>
+            <li>
+              Nanodegree: Digital Marketing. <b> Oct -2021</b>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="right-column">
+        <div className="edu">
+          <h2>Education</h2>
+          <hr></hr>
+
+          <div>
+            <h4>Computer Science</h4>
+            <i>Shaqra University, 2018 – 2022</i>
+            <ul>
+              <li>Continuing education in fronted development</li>
+              <li>
+                The graduation project was building tourism android application.
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>JavaScript Frontend Development Bootcamp</h4>
+            <i>Tuwaiq Academy, Jul 2023- Present</i>
+            <ul>
+              <li>Vanilla JavaScript, html, and CSS, Bootstrap</li>
+              <li>UX & UI using Figma</li>
+            </ul>
+          </div>
+        </div>
+        <div className="exp">
+          <h2>Experience</h2>
+          <hr></hr>
+
+          <div>
+            <h4>Training and Scholarship Center, Sep – Nov 2021</h4>
+            <i>Co-op Trainee</i>
+            <ul>
+              <li>
+                worked on training and scholarship platform: issuing
+                certificates for trainees, make sure trainees attend.
+              </li>
+              <li>
+                Presentation on OneDrive: Explained how to use OneDrive and it
+                is important to use in the facility.
+              </li>
+              <li>Assisting the trainer in virtual training courses.</li>
+            </ul>
+          </div>
+        </div>
+        <div className="volu">
+          <h2>Volunteering</h2>
+          <hr></hr>
+
+          <div>
+            <h4>Masar Initiative, May 2023 - Present</h4>
+            <i>Web Design and UX</i>
+            <ul>
+              <li>Working on website front interfaced.</li>
+              <li>
+                To upgrade UX by analyses which to collect data for improving
+                consumer experience on the website
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
+);
+
+ReactDOM.render(element, react1);
